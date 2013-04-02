50fcd80069655eb17f1e5c730ae1338f0e2d553e
kracekumar <me@kracekumar.com>
2013-04-02 18:24:41 +0530

    added cors support

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    #request.headers.add('Access-Control-Allow-Origin', '*')
+    #raise
+    return render_template('index.html')
 
 @app.route('/json', methods=['POST', 'GET'])
 def return_json():
 	return jsonify({'msg': 'Python'})
 
-app.run('0.0.0.0', port=8888, debug=True)
 
+@app.after_request
+def inject_header(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+app.run('0.0.0.0', port=8888, debug=True)
